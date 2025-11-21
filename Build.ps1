4bf9ab0656a7e91739c4c745d3237b445db3bb59
ChristianHaase <christian.haase.93@gmail.com>
2025-11-21 20:39:23 +0100

    test

--- Build.ps1
+++ Build.ps1
@@ -6,76 +6,79 @@ dotnet --list-sdks
 Write-Output "build: Build started"
 
 Push-Location $PSScriptRoot
-try {
-    if(Test-Path .\artifacts) {
-        Write-Output "build: Cleaning ./artifacts"
-        Remove-Item ./artifacts -Force -Recurse
-    }
 
-    & dotnet restore --no-cache
+Write-Output "build: PR_TRIGGER value: $env:PR_TRIGGER"
 
-    $dvp = [Xml] (Get-Content .\Directory.Version.props)
-    $versionPrefix = $dvp.Project.PropertyGroup.VersionPrefix
+# try {
+#     if(Test-Path .\artifacts) {
+#         Write-Output "build: Cleaning ./artifacts"
+#         Remove-Item ./artifacts -Force -Recurse
+#     }
 
-    Write-Output "build: Package base version is $versionPrefix"
+#     & dotnet restore --no-cache
 
-    $branch = @{ $true = $env:CI_TARGET_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:CI_TARGET_BRANCH];
-    $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:CI_BUILD_NUMBER];
-    $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-    $commitHash = $(git rev-parse --short HEAD)
-    $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+#     $dvp = [Xml] (Get-Content .\Directory.Version.props)
+#     $versionPrefix = $dvp.Project.PropertyGroup.VersionPrefix
 
-    Write-Output "build: Package version suffix is $suffix"
-    Write-Output "build: Build version suffix is $buildSuffix"
+#     Write-Output "build: Package base version is $versionPrefix"
 
-    & dotnet build -c Release --version-suffix=$buildSuffix /p:ContinuousIntegrationBuild=true
-    if($LASTEXITCODE -ne 0) { throw "Build failed" }
+#     $branch = @{ $true = $env:CI_TARGET_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:CI_TARGET_BRANCH];
+#     $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:CI_BUILD_NUMBER];
+#     $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+#     $commitHash = $(git rev-parse --short HEAD)
+#     $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
-    foreach ($src in Get-ChildItem src/*) {
-        Push-Location $src
+#     Write-Output "build: Package version suffix is $suffix"
+#     Write-Output "build: Build version suffix is $buildSuffix"
 
-        Write-Output "build: Packaging project in $src"
+#     & dotnet build -c Release --version-suffix=$buildSuffix /p:ContinuousIntegrationBuild=true
+#     if($LASTEXITCODE -ne 0) { throw "Build failed" }
 
-        if ($suffix) {
-            & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts --version-suffix=$suffix
-        } else {
-            & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts
-        }
-        if($LASTEXITCODE -ne 0) { throw "Packaging failed" }
+#     foreach ($src in Get-ChildItem src/*) {
+#         Push-Location $src
 
-        Pop-Location
-    }
+#         Write-Output "build: Packaging project in $src"
 
-    foreach ($test in Get-ChildItem tests/) {
-        Push-Location $test
+#         if ($suffix) {
+#             & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts --version-suffix=$suffix
+#         } else {
+#             & dotnet pack -c Release --no-build --no-restore  -o ../../artifacts
+#         }
+#         if($LASTEXITCODE -ne 0) { throw "Packaging failed" }
 
-        Write-Output "build: Testing project in $test"
+#         Pop-Location
+#     }
 
-        & dotnet test -c Release --no-build --no-restore
-        if($LASTEXITCODE -ne 0) { throw "Testing failed" }
+#     foreach ($test in Get-ChildItem tests/) {
+#         Push-Location $test
 
-        Pop-Location
-    }
+#         Write-Output "build: Testing project in $test"
 
-    if ($env:DUMMY) {
-        # GitHub Actions will only supply this to branch builds and not PRs. We publish
-        # builds from any branch this action targets (i.e. master and dev).
+#         & dotnet test -c Release --no-build --no-restore
+#         if($LASTEXITCODE -ne 0) { throw "Testing failed" }
 
-        Write-Output "DUMMY value: $env:DUMMY"
+#         Pop-Location
+#     }
 
-        # Write-Output "build: Publishing NuGet package"
+#     if ($env:DUMMY) {
+#         # GitHub Actions will only supply this to branch builds and not PRs. We publish
+#         # builds from any branch this action targets (i.e. master and dev).
 
-        # foreach ($nupkg in Get-ChildItem artifacts/*.nupkg) {
-        #     & dotnet nuget push -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json "$nupkg"
-        #     if($LASTEXITCODE -ne 0) { throw "Publishing failed" }
-        # }
+#         Write-Output "DUMMY value: $env:DUMMY"
 
-        # if (!($suffix)) {
-        #     Write-Output "build: Creating release for version $versionPrefix"
+#         # Write-Output "build: Publishing NuGet package"
 
-        #     iex "gh release create v$versionPrefix --title v$versionPrefix --generate-notes $(get-item ./artifacts/*.nupkg) $(get-item ./artifacts/*.snupkg)"
-        # }
-    }
-} finally {
-    Pop-Location
-}
+#         # foreach ($nupkg in Get-ChildItem artifacts/*.nupkg) {
+#         #     & dotnet nuget push -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json "$nupkg"
+#         #     if($LASTEXITCODE -ne 0) { throw "Publishing failed" }
+#         # }
+
+#         # if (!($suffix)) {
+#         #     Write-Output "build: Creating release for version $versionPrefix"
+
+#         #     iex "gh release create v$versionPrefix --title v$versionPrefix --generate-notes $(get-item ./artifacts/*.nupkg) $(get-item ./artifacts/*.snupkg)"
+#         # }
+#     }
+# } finally {
+#     Pop-Location
+# }
