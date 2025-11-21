984fd3dc59516297b5214e1b414984ee6893964c
ChristianHaase <christian.haase.93@gmail.com>
2025-11-21 12:52:05 +0100

    fix: fix incorrect main branch filter

--- Build.ps1
+++ Build.ps1
@@ -21,7 +21,7 @@ try {
 
     $branch = @{ $true = $env:CI_TARGET_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:CI_TARGET_BRANCH];
     $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:CI_BUILD_NUMBER, 10); $false = "local" }[$NULL -ne $env:CI_BUILD_NUMBER];
-    $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "main" -and $revision -ne "local"]
+    $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)) -replace '([^a-zA-Z0-9\-]*)', '')-$revision"}[$branch -eq "master" -and $revision -ne "local"]
     $commitHash = $(git rev-parse --short HEAD)
     $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
 
@@ -59,7 +59,7 @@ try {
 
     if ($env:NUGET_API_KEY) {
         # GitHub Actions will only supply this to branch builds and not PRs. We publish
-        # builds from any branch this action targets (i.e. main and dev).
+        # builds from any branch this action targets (i.e. master and dev).
 
         Write-Output "build: Publishing NuGet package"
 
