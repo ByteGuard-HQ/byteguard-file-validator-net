5ba28a65e63dd06236f1ec50bd2644ae644d5896
ChristianHaase <christian.haase.93@gmail.com>
2025-11-27 20:34:02 +0100

    refactor: remove unused friendly file size limit
    Thing configuration key should've never been in the core package, but is instead specific for the AspNetCore package. Does require the aspnetcore package to be updated and ensure reference to new versuon of the core package.

--- src/ByteGuard.FileValidator/Configuration/FileValidatorConfiguration.cs
+++ src/ByteGuard.FileValidator/Configuration/FileValidatorConfiguration.cs
@@ -26,24 +26,6 @@ namespace ByteGuard.FileValidator.Configuration
         public long FileSizeLimit { get; set; } = -1;
 
         /// <summary>
-        /// Maximum file size limit in string representation (e.g. "25MB", "2 GB", etc.).
-        /// </summary>
-        /// <remarks>
-        /// Defines the file size limit of files. See <see cref="ByteSize"/> for conversion help.
-        /// Will be ignored if <see cref="FileSizeLimit"/> is defined.
-        /// Spacing (<c>"25 MB"</c> vs. <c>"25MB"</c>) is irrelevant.
-        /// <para>Supported string representation are:
-        /// <ul>
-        /// <li><c>B</c>: Bytes</li>
-        /// <li><c>KB</c>: Kilobytes</li>
-        /// <li><c>MB</c>: Megabytes</li>
-        /// <li><c>GB</c>: Gigabytes</li>
-        /// </ul>
-        /// </para>
-        /// </remarks>
-        public string FriendlyFileSizeLimit { get; set; } = default!;
-
-        /// <summary>
         /// Whether to throw an exception if an unsupported/invalid file is encountered. Defaults to <c>true</c>.
         /// </summary>
         public bool ThrowExceptionOnInvalidFile { get; set; } = true;
