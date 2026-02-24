85f2f130b6c7bbd09fe2fadcad702badf679bf7e
Christian Haase <chaa@detfagligehus.dk>
2026-02-24 10:35:22 +0100

    tests: add unit tests for new odf and ooxml validation structure

--- tests/ByteGuard.FileValidator.Tests.Unit/ConfigurationValidatorTests.cs
+++ tests/ByteGuard.FileValidator.Tests.Unit/ConfigurationValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using ByteGuard.FileValidator.Configuration;
 using ByteGuard.FileValidator.Exceptions;
+using DocumentFormat.OpenXml;
 
 namespace ByteGuard.FileValidator.Tests.Unit;
 
@@ -87,6 +88,60 @@ public class ConfigurationValidatorTests
         {
             SupportedFileTypes = new() { ".jpg" }
         };
+
+        // Act
+        Action act = () => new FileValidator(config);
+
+        // Assert
+        Assert.Throws<ArgumentException>(act);
+    }
+
+    [Fact(DisplayName = "ThrowIfInvalid should throw ArgumentNullException if OdfRules is null")]
+    public void ThrowIfInvalid_OdfRulesIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var config = new FileValidatorConfiguration
+        {
+            SupportedFileTypes = new() { ".odt" },
+            FileSizeLimit = ByteSize.MegaBytes(25)
+        };
+        config.FileTypeRules.OdfRules = null!;
+
+        // Act
+        Action act = () => new FileValidator(config);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact(DisplayName = "ThrowIfInvalid should throw ArgumentNullException if OpenXmlRules is null")]
+    public void ThrowIfInvalid_OpenXmlRulesIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var config = new FileValidatorConfiguration
+        {
+            SupportedFileTypes = new() { ".docx" },
+            FileSizeLimit = ByteSize.MegaBytes(25)
+        };
+        config.FileTypeRules.OpenXmlRules = null!;
+
+        // Act
+        Action act = () => new FileValidator(config);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact(DisplayName = "ThrowIfInvalid should throw ArgumentException if OpenXmlRules.ConformanceVersion is 'None'")]
+    public void ThrowIfInvalid_OpenXmlConformanceVersionIsNone_ShouldThrowArgumentException()
+    {
+        // Arrange
+        var config = new FileValidatorConfiguration
+        {
+            SupportedFileTypes = new() { ".docx" },
+            FileSizeLimit = ByteSize.MegaBytes(25)
+        };
+        config.FileTypeRules.OpenXmlRules.ConformanceVersion = FileFormatVersions.None;
 
         // Act
         Action act = () => new FileValidator(config);
